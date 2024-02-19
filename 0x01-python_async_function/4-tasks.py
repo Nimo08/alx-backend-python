f1a5e1f601c321d59135995cd89f32a200903787
Nimo08 <nimomohamed800@gmail.com>
2024-02-19 16:49:04 +0300

    Editing typing import & task_wait_n function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,17 +6,21 @@ The code is nearly identical to wait_n except task_wait_random is being called.
 
 
 import asyncio
-import typing
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Return the list of all the delays (float values).
     The list of the delays should be in ascending order without using sort()
     because of concurrency.
     """
     delays = []
-    for _ in range(n):
-        delays.append(task_wait_random(max_delay))
-    return sorted(await asyncio.gather(*delays))
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    # Takes a list of tasks and yields futures as they complete
+    for future in asyncio.as_completed(tasks):
+        # Suspends execution of coroutine until future completes
+        delay = await future
+        delays.append(delay)
+    return delays
