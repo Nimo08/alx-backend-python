342531f7398113e2c63c45cf0cacd88b8eac7bc3
Nimo08 <nimomohamed800@gmail.com>
2024-02-19 15:17:44 +0300

    Reverting to initial script

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -17,13 +17,8 @@ async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
     because of concurrency.
     """
     delays = []
-    tasks = []
     for _ in range(n):
-        tasks.append(task_wait_random(max_delay))
-    # Takes a list of tasks and yields futures as they complete
-    for future in asyncio.as_completed(tasks):
-        # Suspends execution of coroutine until future completes
-        delay = await future
+        delay = await task_wait_random(max_delay)
         delays.append(delay)
-    sorted_delays = sorted(delays)
+        sorted_delays = sorted(delays)
     return sorted_delays
