15aa93ad46d1a274ed3b0954198d270765ca7bad
Nimo08 <nimomohamed800@gmail.com>
2024-02-19 12:12:08 +0300

    Return value: float

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -12,8 +12,7 @@ import random
 import typing
 
 
-async def wait_random(max_delay: int = 10) \
-                            -> typing.Union[int, float]:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Waits for a random delay between 0 and max_delay
     (included and float value) seconds and eventually returns it.
