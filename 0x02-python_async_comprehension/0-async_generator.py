0a41c4212fe13cd4ff50d8da34c3c615987e8bfa
Nimo08 <nimomohamed800@gmail.com>
2024-02-20 12:11:05 +0300

    Yield random float directly

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -18,5 +18,4 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        num = random.uniform(0, 10)
-        yield num
+        yield random.uniform(0, 10)
